356f35e1874f7bf359a792096d734a818fb96766
AMF0 <109762993+AMF0@users.noreply.github.com>
2022-12-27 20:03:55 +0200

    Update proj.h

--- proj.h
+++ proj.h
@@ -1,10 +1,10 @@
 #ifndef PROJ_H_INCLUDED
 #define PROJ_H_INCLUDED
 
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-
+#include <math.h>
 void Home();
 void Math_operation();
 void characteristics_of_Matrix();
@@ -20,11 +20,11 @@ void producting();
 void traspose();
 void determinant2_2();
 void determinant3_3();
-void invers ();
-float small_determinant(float n1,float n2,float n3,float n4);
-float determinant(float [][25], float);
-void cofactor(float [][25], float);
-void transpose(float [][25], float [][25], float);
-void inverse ();
+void invers();
+float small_determinant(float n1, float n2, float n3, float n4);
+float determinant(float[][25], float);
+void cofactor(float[][25], float);
+void transpose(float[][25], float[][25], float);
+void inverse();
 
 #endif // PROJ_H_INCLUDED
